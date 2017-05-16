5001f0c0cb16f1734f9a22ab57d9223672ddae02
Christian Parpart <trapni@gmail.com>
2017-05-16 14:21:24 +0200

    use autoconf/automake to build the tool

---
+++ autogen.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+set -e
+
+ROOT=`dirname $0`
+
+FILES=(
+  /ar-lib
+  /aclocal.m4
+  /compile
+  /autom4te.cache
+  /configure
+  /install-sh
+  /missing
+  /depcomp
+  /sysconfig.h.in
+)
+
+if test "$1" == "clean"; then
+  find $ROOT -name Makefile.in -exec rm {} \;
+  for file in ${FILES[*]}; do rm -vrf "${ROOT}${file}"; done
+  exit 0
+fi
+
+autoreconf --verbose --force --install $ROOT
+
+findexe() {
+  for exe in ${@}; do
+    if which $exe &>/dev/null; then
+      echo $exe
+      return
+    fi
+  done
+  echo $1
+}
+
+export CXX=$(findexe $CXX clang++-3.9 clang++-3.8 clang++ g++)
+export CC=$(findexe $CC clang-3.9 clang-3.8 clang gcc)
+export CXXFLAGS="-O0 -g"
+
+echo CXX = $CXX
+echo CC = $CC
+echo CXXFLAGS = $CXXFLAGS
+
+exec $ROOT/configure --prefix="$HOME/usr"
